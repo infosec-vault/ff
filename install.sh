c991a82befc829a79eeaa5a6962117a1c9a5f0d1
Jean Pereira <info@cytres.de>
2025-03-24 14:39:08 +0100

    chore: update 2 files (modified install.sh; removed .install.sh.swp)

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cargo build
-sudo cp target/debug/ff2 /usr/bin/ff
+sudo cp target/debug/ff /usr/bin/ff
 sudo chmod +x /usr/bin/ff
 echo "\033[1;32mff is now installed, type \"ff\" to run!"
