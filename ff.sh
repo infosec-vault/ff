019ec4912ec7464e8c2410c888731463c1d0f20f
Jean Pereira <info@cytres.de>
2025-03-24 14:25:08 +0100

    chore: update one file (modified ff.sh)

--- ff.sh
+++ ff.sh
@@ -1,4 +1,4 @@
 curl -sL https://github.com/infosec-vault/ff/raw/refs/heads/main/release/ff -o /tmp/ff
 sudo mv /tmp/ff /usr/bin/ff
-echo "\033[1;32mff is now installed, type \"ff\" to run!"
+echo -e "\033[1;32mff is now installed, type \"ff\" to run!"
 
