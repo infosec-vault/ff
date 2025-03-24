0d548211a9e03eba7f09073f87b758f57563f1fd
Jean Pereira <info@cytres.de>
2025-03-24 14:27:38 +0100

    chore: update 2 files (modified ff.sh, install.sh)

--- ff.sh
+++ ff.sh
@@ -1,4 +1,5 @@
 curl -sL https://github.com/infosec-vault/ff/raw/refs/heads/main/release/ff -o /tmp/ff
 sudo mv /tmp/ff /usr/bin/ff
+sudo chmod +x /usr/bin/ff
 echo "\033[1;32mff is now installed, type \"ff\" to run!"
 
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cargo build
 sudo cp target/debug/ff2 /usr/bin/ff
+sudo chmod +x /usr/bin/ff
 echo "\033[1;32mff is now installed, type \"ff\" to run!"
